f5d92245426862b1d4a74475aa41d44fec1f7d6f
Peter Esselius <pepp@me.com>
2024-12-25 22:28:30 +0100

    hub attached io

--- duplo/decoder.py
+++ duplo/decoder.py
@@ -1,7 +1,16 @@
-from construct import Struct, Int8ul
+from construct import Struct, Int8ul, Int16ul, Int32sl
 
 Header = Struct(
     "length" / Int8ul,
     "hub_id" / Int8ul,
     "message_type" / Int8ul,
+)
+
+HubAttachedIO = Struct(
+    "header" / Header,
+    "port" / Int8ul,
+    "event" / Int8ul,
+    "io_type_id" / Int16ul,
+    "hardware_revision" / Int32sl,
+    "software_revision" / Int32sl,
 )
--- duplo/test_decoder.py
+++ duplo/test_decoder.py
@@ -1,4 +1,4 @@
-from .decoder import Header
+from .decoder import Header, HubAttachedIO
 
 def test_parse_header():
     # Length = 15, Hub ID = 0x00, Message Type = 0x04
@@ -10,4 +10,18 @@ def test_parse_header():
     assert parsed.hub_id == 0
     assert parsed.message_type == 0x04
 
+def test_parse_hub_attached_io_msg():
+    data = b'\x0f\x00\x04\x14\x01\x14\x00\x01\x00\x00\x00\x01\x00\x00\x00'
+
+    parsed = HubAttachedIO.parse(data)
+
+    assert parsed.header.length == 15
+    assert parsed.header.hub_id == 0
+    assert parsed.header.message_type == 0x04
+
+    assert parsed.port == 20
+    assert parsed.event == 1
+    assert parsed.io_type_id == 20
+    assert parsed.hardware_revision == 1
+    assert parsed.software_revision == 1
 
