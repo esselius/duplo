ddbf3462ca61a4b691f8e3eecca479f8abe7a2f0
Peter Esselius <pepp@me.com>
2024-12-30 07:55:17 +0100

    reset project

--- services/ble_manager.py
+++
@@ -1,9 +0,0 @@
-from bleak import BleakScanner
-
-
-class BLEManager:
-    def __init__(self, scanner: BleakScanner):
-        self.scanner = scanner
-
-    async def scan_for_devices(self):
-        return await self.scanner.discover()
